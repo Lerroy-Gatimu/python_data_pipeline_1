1f304496e8a6474fa3d108417cb377c89da68080
Lerroy Gatimu <lerroygatimu@gmail.com>
2026-03-18 12:55:46 +0300

    Updated docker files

--- config/config.py
+++ config/config.py
@@ -5,12 +5,12 @@ load_dotenv()
 
 class Config:
     # Database
-    DB_HOST = os.getenv("DB_HOST")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = int(os.getenv("DB_PORT", 3306))
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_NAME = os.getenv("DB_NAME")
-    
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_NAME = os.getenv("DB_NAME", "python_data_pipeline_1")
+
     # Paths
     RAW_DATA_PATH = "data/raw/sales_data.csv"
     LOG_FILE = "logs/pipeline.log"
